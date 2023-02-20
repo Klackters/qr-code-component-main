6d46c393f07243e0f6618e79351834976bce8760
brenofflank <107095857+Klackters@users.noreply.github.com>
2023-02-20 15:56:21 -0300

    up correction

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -4,14 +4,14 @@ const logo = require("../images/image-qr-code.png")
 export default function QRCode() {
   return(
     <section className="bg-sky-100 h-screen w-full flex place-content-center">
-      <div className="bg-white flex flex-col text-center h-1/2 self-center rounded-xl">
+      <div className="bg-white flex flex-col text-center h-1/2 self-center rounded-xl shadow-2xl">
         <div className="flex w-80 h-fit m-4">
           <img src={logo} alt="QRCode" className="rounded-xl" />
         </div>
-        <div className="flex self-center text-2xl font-bold">
+        <div className="flex self-center text-2xl font-bold m-1 text-sky-900">
           <h1>Improve your front-end <br /> skills by building projects</h1>
         </div>
-        <div className="flex self-center text-xl font-thin pt-6">
+        <div className="flex self-center text-xl font-thin m-5 text-slate-400">
           <h1>Scan the QR code to visit Frontend <br /> Mentor and take your conding skills to<br /> the next level</h1>
         </div>
       </div>
